091f1e47063940dbe2d45c73a43addeff1d7995a
Ross Nelson <rossn430@gmail.com>
2024-06-29 16:59:36 -0500

    More CSS Styling

--- public/main.js
+++
@@ -1,7 +0,0 @@
-let root = document.querySelector(":root");
-let button = document.querySelector("button");
-
-button.addEventListener('click', () => {
-  event.preventDefault();
-  root.classList.toggle('dark');
-})
