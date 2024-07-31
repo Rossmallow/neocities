c4d72429eb1eef61f670636acbe929599faa0530
Ross Nelson <rossn430@gmail.com>
2024-07-30 20:27:47 -0500

    Update guestbook logic

--- content/guestbook/index.md
+++ content/guestbook/index.md
@@ -8,17 +8,21 @@ draft: false
 
 ## {{< param title >}}
 
+### The Guestbook is currently non-functional due to mine being a new account. Please see this [reddit post](https://www.reddit.com/r/neocities/comments/1acx6lu/error_putting_statuscafe_into_my_website/).
+
 Creating this guestbook with this [tutorial](https://goblin-heart.net/sadgrl/learn/articles/create-webform-discord)
 
 <form>
-  <label>Name:</label><br>
-  <input id="name" type="text" placeholder="Geddy Lee" required><br>
-  <label>Contact (E-Mail, Instagram, etc.):</label><br>
-  <input id="contact" type="text" placeholder="geddylee@rush.com"><br>
-  <label>Message:</label><br>
-  <textarea id="message" rows="4" cols="50" placeholder="Hey Ross, cool website. I dig it!" required></textarea>
-  <span id="counter">1950/1950</span><br>
-  <button id="send" type="submit">Send</button>
+  <label id="name_label">Name:</label><br>
+  <input id="name" type="text" placeholder="Geddy Lee"><br>
+  <label>Contact (Website, E-Mail, Instagram, etc.):</label><br>
+  <input id="contact" type="text" placeholder="Rush.com"><br>
+  <label id="msg_label">Message:</label><br>
+  <textarea id="message" rows="4" cols="50" placeholder="Hey Ross, cool website. I dig it!"></textarea>
+  <span id="counter">xxxx/xxxx</span><br>
+  <button id="send" type="button">Send</button>
 </form>
+
+Be the first to submit an entry!
 
 <script src="/guestbook.js"></script>
--- static/guestbook.js
+++ static/guestbook.js
@@ -4,9 +4,11 @@ let name_input = document.getElementById("name");
 let contact_input = document.getElementById("contact");
 let msg_input = document.getElementById("message");
 let count_span = document.getElementById("counter");
-const max_name_chars = 20;
+const max_name_chars = 30;
 const max_contact_chars = 30;
-const max_msg_chars = 2000 - max_name_chars - max_contact_chars; // Discord's character limit is 2,000 characters
+const max_msg_chars = 2000; // Discord's character limit is 2000 characters
+
+count_span.textContent = max_msg_chars + "/" + max_msg_chars;
 
 name_input.setAttribute("maxlength", max_name_chars);
 contact_input.setAttribute("maxlength", max_contact_chars);
@@ -31,28 +33,62 @@ msg_input.addEventListener("input", countCharacters);
 
 // Below is from https://goblin-heart.net/sadgrl/learn/articles/create-webform-discord
 //    https://codepen.io/sadness97/pen/YzEgxox?editors=1010
-document.getElementById('send').onclick = function() {
+let button = document.getElementById("send")
+button.onclick = function() {
+  let name_label = document.getElementById("name_label");
+  let msg_label = document.getElementById("msg_label");
+
+  // var datetime = new Date().toLocaleString("en-US");
+  var name = name_input.value;
+  var contact = contact_input.value;
+  var message = msg_input.value;
+
+  name_label.style.color = "black";
+  msg_label.style.color = "black";
+
+  if (name == "" || message == "") {
+    if (name == "") {
+      name_label.style.color = "red";
+    }
+    if (message == "") {
+      msg_label.style.color = "red";
+    }
+    alert('Please fill the required fields.');
+    return;
+  }
+  else {
   var request = new XMLHttpRequest();
   request.open(
     "POST",
     "https://discord.com/api/webhooks/1267673255397949471/Blj_yxH4ydEmS9Gbmo3MQBiujHJm9KPCeNrsR38vHlMADTl_EO4pI6yJDFUjLocfjZTR");
   request.setRequestHeader('Content-type', 'application/json');
-  // var datetime = new Date().toLocaleString("en-US");
-  var name = document.getElementById("name").value;
-  var contact = document.getElementById("contact").value;
-  var message = document.getElementById("message").value;
 
-  var discord_msg = { // add up to 2,000 characters (Discord's character limit)
+  // When the request has been sent and gotten a response
+  request.onreadystatechange = function() {
+    if (request.readyState == XMLHttpRequest.DONE) {
+        if (request.status == 204) {
+          // Gives feedback and clears the form fields after sucessful submission
+          alert('Submission recieved. Thanks for your message!')
+          name_input.value = '';
+          contact_input.value = '';
+          msg_input.value = '';
+          countCharacters()
+        }
+        else if (request.status == 0) {
+          alert('There was a problem submitting your message. Please try again later. ' +
+                'If this persists, please contact the webmaster.')
+        }
+        else {
+          alert("Unexpected response: " + request.status);
+        }
+    }
+  }
+
+  var discord_msg = {
     username: name + " - " + contact,
     content: message
   }
+
   request.send(JSON.stringify(discord_msg));
-
-  // Must fix the below to only display on sucessful submission
-  // alert('Thanks, your message has been sent.'); // Gives feedback that message has been sent
-
-  // clears the form fields after submission
-  name_input.value = '';
-  contact_input.value = '';
-  msg_input.value = '';
+  }
 }
